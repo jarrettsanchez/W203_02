c3340c469707b891c18b07b8c629736bd4f5d408
Jarrett Sanchez <133571345+jarrettsanchez@users.noreply.github.com>
2024-08-30 18:46:53 +1200

    initialised next frontend

--- speed_backend/src/app.module.ts
+++ speed_backend/src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+
 @Module({
-imports: [
-ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
-controllers: [AppController],
-providers: [AppService],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
